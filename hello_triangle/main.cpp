3bfe527f7daf456021d0439ff1369044dfc3105a
Daniel Young <young.daniel345@gmail.com>
2018-01-17 10:46:04 -0500

    end of class fully working triangle animation

--- hello_triangle/main.cpp
+++ hello_triangle/main.cpp
@@ -1,6 +1,6 @@
 #include <glad/glad.h>      // Include glad before gl.h, since that includes the OpenGL header includes
 #include <GLFW/glfw3.h>
-
+#include <cmath>
 #include "Shader.h"
 
 #include <iostream>
@@ -45,12 +45,23 @@ int main() {
     int triangleVAO = loadTriangle();
 
     while (!glfwWindowShouldClose(window)) {
+        // input
         processInput(window);
-
+        // render
+        // clear the colorbuffer
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-
-        drawTriangle(triangleProgram, triangleVAO);
-
+        // be sure to activate the shader
+        glUseProgram(triangleProgram.ID);
+        // update the uniform color
+        float timeValue = glfwGetTime();
+        float greenValue = sin(timeValue) / 2.0f + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(triangleProgram.ID, "ourColor");
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+        // now render the triangle
+        glBindVertexArray(triangleVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // swap buffers and poll IO events
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
